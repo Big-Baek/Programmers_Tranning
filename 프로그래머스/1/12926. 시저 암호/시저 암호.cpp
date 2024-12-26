93cce7c49e4c458899a6ef49a5ea96336883a451
백의현 <euihyun1188@gmail.com>
2024-12-26 14:50:22 +0900

    Update 시저 암호.cpp

--- 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -10,6 +10,10 @@ string solution(string s, int n) {
         if (isalpha(s[i])) { // 알파벳인지 확인
             char base = isupper(s[i]) ? 'A' : 'a';
             s[i] = (s[i] - base + n) % 26 + base;
+            	/*원본에서 첫 알파벳 숫자를 빼면 25 이하의 숫자가 나온다
+	거기서 밀어버릴 n을 더하면 원본에서 밀 최종 n값이 나온다.
+	근데 25이상의 숫자가 나올수도 있기에 %26을 해둔다
+	원본 (A든 a든)에서 최종n을 더하면 원하는 값이 나온다.*/
         }
     }
     return s;
