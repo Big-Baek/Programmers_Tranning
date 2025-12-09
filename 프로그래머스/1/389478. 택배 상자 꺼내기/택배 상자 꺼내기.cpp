810bdcb7e535639f6e9a2481110d2973a2fe7c28
백의현 <euihyun1188@gmail.com>
2025-12-09 21:57:50 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -5,19 +5,11 @@ using namespace std;
 
 int solution(int n, int w, int num) {
 	int answer = 0;
-
 	while (num <= n)
 	{
-		if (num % w != 0)
-		{
-			num = num + 1 + (2 * (w - (num % w)));
-		}
-		else
-		{
-			num = num + 1;
-		}
+		if (num % w != 0) num = num + 1 + (2 * (w - (num % w)));
+		else num = num + 1;
 		answer++;
 	}
-
 	return answer;
 }
