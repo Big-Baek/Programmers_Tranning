8530d1084310384a3cf03b125069e3c6521df996
백의현 <euihyun1188@gmail.com>
2026-03-04 01:00:52 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.49 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -8,31 +8,34 @@ string ChangeVal(int number, int Jin)
 {
 	if (number == 0) return "0";
 
-	string digits = "0123456789ABCDEF"; //16진수 지원
+	string digits = "0123456789ABCDEF"; //16진수까지 지원
 	string answer;
-	while (number > 0)
+
+	while (number > 0)//나눈값이 0이하가 될때까지 반복
 	{
 		answer += digits[number % Jin];
 		number /= Jin;
 	}
-	reverse(answer.begin(), answer.end());
+
+	reverse(answer.begin(), answer.end()); //역순으로 변환
 	return answer;
 }
 
-string solution(int n, int t, int m, int p)//진수, 구할 숫자길이, 총 멤버, 본인 차례 
+string solution(int n, int t, int m, int p)
 {
 	string answer = "";
-	int num = 0;
 
-	string TotalNum = "";
+	int num = 0;
+	string TotalNum = "";//구할 전체숫자
+
 	for (int i = 0; i <= m*t; i++) //모든 숫자 구함
 	{
-		TotalNum += ChangeVal(num, n);
+		TotalNum += ChangeVal(num, n);//진수변환함수
 		num++;
 	}
 
 	int count = p;
-	for (int i = 0; i < t; i ++)
+	for (int i = 0; i < t; i ++)//전체숫자준 쓸 숫자만 뽑기
 	{
 		answer += TotalNum[p-1];
 		p += m;
